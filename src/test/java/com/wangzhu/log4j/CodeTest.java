c00736318c07104fd5e9eea1a52c03a60f50146b
wang.zhu <wang.zhu@immomo.com>
2018-04-15 17:46:59 +0800

    log4j配置方式：xml和代码

---
+++ src/test/java/com/wangzhu/log4j/CodeTest.java
@@ -0,0 +1,63 @@
+package com.wangzhu.log4j;
+
+import org.apache.log4j.*;
+import org.junit.Test;
+
+/**
+ * Created by wangzhu on 2018/4/15 下午5:22.
+ */
+public class CodeTest {
+    static {
+        try {
+            System.setProperty("log4j.defaultInitOverride", "true");
+            Logger rootLogger = Logger.getRootLogger();
+            rootLogger.setLevel(Level.DEBUG);
+            rootLogger.removeAllAppenders();
+            rootLogger.addAppender(new ConsoleAppender(new PatternLayout("%d{yyyy-MM-dd HH:mm:ss,SSS} [%t] %-5p %l - %m%n")));
+
+            RollingFileAppender rollingFileAppender1 = new RollingFileAppender();
+            rollingFileAppender1.setLayout(new PatternLayout("%m%n"));
+            rollingFileAppender1.setFile("log4jTest/log_1.txt");
+            rollingFileAppender1.setAppend(true);
+            rollingFileAppender1.setBufferedIO(true);
+            rollingFileAppender1.activateOptions();
+
+            Logger log1 = Logger.getLogger("log1");
+            log1.removeAllAppenders();
+            log1.addAppender(rollingFileAppender1);
+            log1.setAdditivity(false);
+            log1.setLevel(Level.INFO);
+
+
+            RollingFileAppender rollingFileAppender2 = new RollingFileAppender(new PatternLayout("%m%n"), "log4jTest/log_2.txt", true);
+
+            Logger log2 = Logger.getLogger("log2");
+            log2.removeAllAppenders();
+            log2.addAppender(rollingFileAppender2);
+            log2.setAdditivity(false);
+            log2.setLevel(Level.INFO);
+
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    LogManager.shutdown();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final org.slf4j.Logger logger1 = org.slf4j.LoggerFactory.getLogger("log1");
+    private static final org.slf4j.Logger logger2 = org.slf4j.LoggerFactory.getLogger("log2");
+
+    @Test
+    public void test(){
+        for(int i = 0;i < 10000;i ++) {
+            logger1.info("{}", i);
+        }
+        for(int i = 0;i < 10000;i ++) {
+            logger2.info("{}", i);
+        }
+    }
+}
---
+++ src/test/java/com/wangzhu/log4j/PropertiesTest.java
@@ -0,0 +1,38 @@
+package com.wangzhu.log4j;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by wangzhu on 2018/4/15 下午5:27.
+ */
+public class PropertiesTest {
+    static{
+        System.setProperty("log4j.defaultInitOverride", "true");
+        PropertyConfigurator.configure(Thread.currentThread().getContextClassLoader().getResource("log4j.properties"));
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                LogManager.shutdown();
+            }
+        });
+    }
+    private static final Logger logger = LoggerFactory.getLogger(PropertiesTest.class);
+
+    private static final Logger fileLog1 = LoggerFactory.getLogger("f1");
+    private static final Logger fileLog2 = LoggerFactory.getLogger("f2");
+    private static final Logger fileLog3 = LoggerFactory.getLogger("f3");
+
+    @Test
+    public void test() {
+        for (int i = 0; i < 10000; i++) {
+            logger.info("{}:time:{}", i, System.currentTimeMillis());
+            fileLog1.info("{}:hello", i);
+            fileLog2.info("{}:world", i);
+            fileLog3.info("{}:welcome", i);
+        }
+    }
+}
